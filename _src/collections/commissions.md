e8c6a98f9016e8e885280f36d5a8eed3fdcabb48
Aque <aque.iriascend@gmail.com>
2026-06-22 19:18:49 +0200

    gallery code

--- _src/collections/commissions.md
+++ _src/collections/commissions.md
@@ -3,4 +3,13 @@ layout: _page.html
 tags: collections
 displayName: Commissions
 ---
-Under construction: Archive of art I've commissioned.
+
+// based on https://stefanbohacek.com/blog/making-a-free-image-gallery-with-neocities-org/
+<div id="gallery">
+    {% for image in myCollections.commissions %}<figure class="pswp-gallery__item">
+        <a href="{{image.src}}" data-pswp-width="{{image.width}}" data-pswp-height="{{image.height}}" target="_blank">
+            <img src="{{image.src}}" title="{{image.title}}" alt="{{image.alt}}" /></a>
+            <figcaption class="pswp-caption-content">{{image.caption}}, by <a href="{{image.authorLink}}" target="_blank">{{image.artist}}</a></figcaption>
+        </a>
+    </figure>{% endfor %}
+</div>
--- eleventy.config.js
+++ eleventy.config.js
@@ -70,6 +70,44 @@ module.exports = async function (eleventyConfig) {
         </div>`;
     });
 
+    // https://stefanbohacek.com/blog/making-a-free-image-gallery-with-neocities-org/
+    const sharp = require('sharp');
+    const Image = require('@11ty/eleventy-img');
+
+    const GALLERY_IMAGE_WIDTH = 192;
+    const LANDSCAPE_LIGHTBOX_IMAGE_WIDTH = 2000;
+    const PORTRAIT_LIGHTBOX_IMAGE_WIDTH = 720;
+
+    async function galleryImageShortcode(src, alt) {
+        let lightboxImageWidth = LANDSCAPE_LIGHTBOX_IMAGE_WIDTH;
+
+        const metadata = await sharp(src).metadata();
+
+        if (metadata.height > metadata.width) {
+            lightboxImageWidth = PORTRAIT_LIGHTBOX_IMAGE_WIDTH;
+        }
+
+        const options = {
+            formats: ['jpeg'],
+            widths: [GALLERY_IMAGE_WIDTH, lightboxImageWidth],
+            urlPath: "/assets/images/commissions",
+            outputDir: './_site/assets/images/commissions/'
+        }
+
+        const genMetadata = await Image(src, options);
+
+        return `
+        <a href="${genMetadata.jpeg[1].url}" 
+        data-pswp-width="${genMetadata.jpeg[1].width}" 
+        data-pswp-height="${genMetadata.jpeg[1].height}" 
+        target="_blank">
+            <img src="${genMetadata.jpeg[0].url}" alt="${alt}" />
+        </a>
+    `.replace(/(\r\n|\n|\r)/gm, "");;
+    }
+
+    eleventyConfig.addLiquidShortcode('galleryImage', galleryImageShortcode);
+
     // FILTERS
 
     // date filters
